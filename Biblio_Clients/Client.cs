1bbcaf01c3c8a517ab28a669242011f7e49f62d9
LorraineLscr <lorraiine76@hotmail.fr>
2022-09-12 14:36:30 +0200

    Ajoutez des fichiers projet.

---
+++ Biblio_Clients/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+
+namespace Biblio_Clients
+{
+    public class Client
+    {
+        // Constructor
+        public Client() { }
+        public Client(int id, string name, string city)
+        {
+            Id = id;
+            _name = name;
+            City = city;
+
+        }
+        // Propriètés
+        public int Id { get; set; } = 0;
+
+        private string _name = "Undefined";
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentNullException();
+                _name = value;
+            }
+        }
+        public string City { get; set; }
+    }
+}
---
+++ Biblio_Clients/ClientManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_Clients
+{
+    internal class ClientManager
+    {
+        public static void Main()
+        {
+
+            // Créer une liste de clients
+            List<Client> clients = new List<Client>();
+
+            clients.Add(new Client() { Id = 1, Name = "ELSYLOG", City = "Denestanville" });
+            clients.Add(new Client() { Id = 2, Name = "INTERMARCHE", City = "Saint-Nicolas-d'Aliermont" });
+            clients.Add(new Client() { Id = 3, Name = "AGORINFO", City = "Isneauville" });
+
+            // Parcourir la liste avec la boucle foreach
+            foreach (Client item in clients)
+            {
+                Console.WriteLine("Id : " + item.Id + " " + item.Name + " City : " + item.City + "\n");
+            }
+
+            // Rechercher un client avec la méthode Find
+            Console.WriteLine("Rechercher : ");
+            string searchString = Console.ReadLine();
+            Client c = clients.Find(c => c.Name == searchString);
+            if (c != null)
+                Console.WriteLine("Id : " + c.Id + " Name : " + c.Name + " City : " + c.City + "\n");
+            else
+                Console.WriteLine("Pas trouvé");
+
+
+        }
+    }
+}
